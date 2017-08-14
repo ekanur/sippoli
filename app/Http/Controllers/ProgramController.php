7d4d773458d6ca984b9bc247d940914a4cd1576d
Eka Nur Ahmad R <ekanur@um.ac.id>
2017-08-14 16:53:00 +0700

    simpan program makro

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Program;
+use App\Sesi_latihan;
 
 class ProgramController extends Controller
 {
@@ -12,12 +13,21 @@ class ProgramController extends Controller
     }
 
     public function simpan(Request $request){
+        $request->mulai_program = date('Y-m-d', strtotime($request->mulai_program));
+        $request->berakhir_program = date('Y-m-d', strtotime($request->berakhir_program));
+        $siklus_makro = array(
+                                        "persiapan_umum"=>$request->persiapan_umum,
+                                        "persiapan_khusus"=>$request->persiapan_khusus,
+                                        "pra_kompetisi"=>$request->pra_kompetisi,
+                                        "kompetisi"=>$request->kompetisi,
+                                        "transisi"=>$request->transisi);
         $program = new Program;
         $program->nama = $request->nama;
         $program->jangka_durasi = $request->jangka_durasi;
         $program->pelatih_id = 1;
         $program->mulai_program = $request->mulai_program;
         $program->berakhir_program = $request->berakhir_program;
+        $program->siklus_makro = json_encode($siklus_makro);
 
         $program->save();
 
@@ -34,7 +44,8 @@ class ProgramController extends Controller
 
     public function edit($id_program){
         $program = Program::findOrFail($id_program);
-    	return view("program.index");
+        // dd(intval(json_decode($program->siklus_makro)->persiapan_khusus));
+    	return view("program.index", compact('program'));
     }
 
     public function pilihAtlet($id_program){
@@ -46,10 +57,21 @@ class ProgramController extends Controller
     }
 
     public function sesiLatihan($id_program, $id_siklus_mikro){
-    	return view("program.sesi_latihan");
+    	return view("program.sesi_latihan", compact('id_program', 'id_siklus_mikro'));
     }
 
     public function programMakanan($id_program){
     	return view("program.makanan");
+    }
+
+    public function simpanSesiLatihan($siklus_mikro_id, Request $request){
+        $request->tanggal = date("Y-m-d", strtotime($request->tanggal));
+        // $json_materi_latihan = 
+        $sesi_latihan = new Sesi_latihan;
+        $sesi_latihan->siklus_mikro_id = $siklus_mikro_id;
+        $sesi_latihan->tahapan = $request->tahapan;
+        $sesi_latihan->tanggal = $request->tanggal;
+        $sesi_latihan->kriteria_volume_intensitas = $request->kriteria_v_i;
+        $sesi_latihan->tanggal = $request->tanggal;
     }
 }
